f92ded65be5aa66d2e23c2c74c524829d181460d
lucaslmp2 <lucaspachecolp2@gmail.com>
2025-04-27 13:49:29 -0300

    Ajustes

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -2,6 +2,7 @@ extends Control
 
 @export var game_scene: String = "res://Level/fase_1.tscn"  # Define a cena do jogo
 @export var menu_scene: String = "res://Level/main_menu.tscn"  # Define a cena do menu principal
+@onready var click: AudioStreamPlayer2D = $click
 
 func _ready() -> void:
 	$VBoxContainer/ButtonJogar.pressed.connect(_on_jogar_pressed)
@@ -13,10 +14,16 @@ func _input(event: InputEvent) -> void:
 		get_tree().change_scene_to_file(menu_scene)  # Volta para o menu principal
 
 func _on_jogar_pressed() -> void:
+	click.play()
+	await get_tree().create_timer(1.0).timeout
 	get_tree().change_scene_to_file(game_scene)  # Vai para o jogo
 
 func _on_opcoes_pressed() -> void:
+	click.play()
+	await get_tree().create_timer(1.0).timeout
 	print("Opções ainda não implementadas!")  # Podemos adicionar um menu depois
 
 func _on_sair_pressed() -> void:
+	click.play()
+	await get_tree().create_timer(1.0).timeout
 	get_tree().quit()  # Fecha o jogo
